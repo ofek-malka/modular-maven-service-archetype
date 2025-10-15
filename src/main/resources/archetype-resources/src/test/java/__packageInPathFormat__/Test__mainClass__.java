56408e098ca834a8f9695d32127ff4e9942f9fc6
Ofek Malka <ofekmalka.om@gmail.com>
2025-10-15 12:41:31 +0300

    Initial commit - modular Maven archetype

---
+++ src/main/resources/archetype-resources/src/test/java/__packageInPathFormat__/Test__mainClass__.java
@@ -0,0 +1,16 @@
+package ${package};
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class Test${mainClass} {
+
+    @Test
+    void exampleTest() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
+
+
+
+
---
+++ target/classes/archetype-resources/src/main/java/__packageInPathFormat__/__mainClass__.java
@@ -0,0 +1,10 @@
+package ${package};
+
+import java.util.*; 
+import java.io.*;  
+
+public class ${mainClass} {
+    public static void main(String[] args) {
+        System.out.println("Hello from modular Maven service archetype!");
+    }
+}
